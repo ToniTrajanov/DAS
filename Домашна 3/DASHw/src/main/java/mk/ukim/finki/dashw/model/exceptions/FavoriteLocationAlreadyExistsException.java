753699f694d8937b9807bdba84d48d6c9df3819d
ToniTrajanov <62966387+ToniTrajanov@users.noreply.github.com>
2022-12-25 15:00:47 +0100

    Delete Домашна 3/DASHw/src/main directory

--- Домашна 3/DASHw/src/main/java/mk/ukim/finki/dashw/model/exceptions/FavoriteLocationAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.dashw.model.exceptions;
-
-public class FavoriteLocationAlreadyExistsException extends RuntimeException{
-    public FavoriteLocationAlreadyExistsException() {
-        super("Location is already added.");
-    }
-}
