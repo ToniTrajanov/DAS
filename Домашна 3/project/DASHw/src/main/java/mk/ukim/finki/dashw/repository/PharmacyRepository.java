440871e63c93587e92521534bd2b4b242d0f0aec
Toni <toni.trajanov@students.finki.ukim.mk>
2022-12-25 14:08:16 +0100

    Implemented some functionalities for hospital, pharmacies etc..

--- Домашна 3/project/DASHw/src/main/java/mk/ukim/finki/dashw/repository/PharmacyRepository.java
+++
@@ -1,9 +0,0 @@
-package mk.ukim.finki.dashw.repository;
-
-import mk.ukim.finki.dashw.model.Pharmacy;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
-}
